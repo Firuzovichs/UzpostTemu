0ebf4495180bd5c71addab4854b65ef1940654ec
Firuzovichs <firuzovichs@gmail.com>
2025-03-20 17:27:10 +0500

    admin

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .models import Order
-admin.site.register(Order)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("order_number", "status", "warehouse_name")  # Jadvaldagi ustunlar
+    list_filter = ("status", "warehouse_name")  # Filtr bo‘limi
+    search_fields = ("order_number", "warehouse_name")  # Qidirish imkoniyati
