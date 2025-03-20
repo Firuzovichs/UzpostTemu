b39bd158224451e5a9a2182dd9cad1d7f1df2d4f
Firuzovichs <firuzovichs@gmail.com>
2025-03-20 17:51:25 +0500

    admin1

--- data/admin.py
+++ data/admin.py
@@ -4,5 +4,5 @@ from .models import Order
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("order_number", "status", "warehouse_name")  # Jadvaldagi ustunlar
-    list_filter = ("status", "warehouse_name")  # Filtr bo‘limi
-    search_fields = ("order_number", "warehouse_name")  # Qidirish imkoniyati
+    list_filter = ("order_number","status", "warehouse_name")  # Filtr bo‘limi
+    search_fields = ("order_number","status", "warehouse_name")  # Qidirish imkoniyati
