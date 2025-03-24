5484520c28e2d25b10c90797040a9f7c69bd9f21
Firuzovichs <firuzovichs@gmail.com>
2025-03-24 16:09:19 +0500

    setting

--- temu/settings.py
+++ temu/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'temu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'temu',
+        'USER': 'usertemu',
+        'PASSWORD': '9382',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -130,7 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
