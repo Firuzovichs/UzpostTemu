6eb52ff54faa84fb08adca24a02c2e28666c88ce
Firuzovichs <firuzovichs@gmail.com>
2025-03-24 16:03:49 +0500

    admin

--- barcode/admin.py
+++ barcode/admin.py
@@ -1,4 +1,9 @@
+
 from django.contrib import admin
 from .models import MailItem
-# Register your models here.
-admin.site.register(MailItem)
+
+@admin.register(MailItem)
+class OrderAdmin(admin.MailItem):
+    list_display = ("barcode", "batch", "last_event_name")  # Jadvaldagi ustunlar
+    list_filter = ("barcode","batch", "last_event_name")  # Filtr bo‘limi
+    search_fields = ("barcode","batch", "last_event_name")  # Qidirish imkoniyati
