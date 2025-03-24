da72187e2781f27b2e25b6b157a1831c5502f489
Firuzovichs <firuzovichs@gmail.com>
2025-03-24 16:18:40 +0500

    Removed

--- barcode/admin.py
+++ barcode/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import MailItem
 
 @admin.register(MailItem)
-class OrderAdmin(admin.MailItem):
+class OrderAdmin(admin.ModelAdmin):
     list_display = ("barcode", "batch", "last_event_name")  # Jadvaldagi ustunlar
     list_filter = ("barcode","batch", "last_event_name")  # Filtr bo‘limi
     search_fields = ("barcode","batch", "last_event_name")  # Qidirish imkoniyati
