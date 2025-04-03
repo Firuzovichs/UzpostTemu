93214379c19dc77a2593f9a9d836c23f4ad1dc44
Firuzovichs <firuzovichs@gmail.com>
2025-04-03 17:03:21 +0500

    fd1233

--- barcode/views.py
+++ barcode/views.py
@@ -26,7 +26,7 @@ from django.db.models import Sum, Count
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import ObtainTokenSerializer, UserSerializer
+from .serializers import  UserSerializer
 from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib.auth import authenticate
