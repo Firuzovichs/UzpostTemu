5655c7524ca1aa51d79a88d1212521d834add0e3
Firuzovichs <firuzovichs@gmail.com>
2025-05-08 15:08:59 +0500

    Update views.py

--- barcode/views.py
+++ barcode/views.py
@@ -62,14 +62,7 @@ class MailItemStatsAPIView(APIView):
     last_event_name__contains=["returning_to_origin"]
 ).count()
 
-        other_count = MailItem.objects.annotate(
-            last_event=RawSQL(
-                "last_event_name[array_length(last_event_name, 1)]",
-                []
-            )
-        ).exclude(
-            Q(last_event="On way") | Q(last_event="completed")
-        ).count()
+        other_count = total - completed - return_status
 
         def percentage(count):
             return round((count / total) * 100, 2) if total > 0 else 0
